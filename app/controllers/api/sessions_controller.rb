b4c325cc0b94dce656e355d9fd1490a6c583153b
Syrie Bianco <syriebianco@gmail.com>
2017-07-17 15:10:44 -0700

    Complete sessions controller logic

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,25 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(
+    params[:user][:username],
+    params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      render "api/sessions/show"
+    else
+      render(json: ["Invalid login credentials."], status: 401)
+    end
   end
 
   def destroy
+    @user = current_user
+    if @user
+      logout
+      render "api/session"
+    else
+      render(json: ["Invalid login credentials."], status: 401)
+    end
   end
 end
