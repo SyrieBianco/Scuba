55bf558c3c90b9126d5d0826d69b2f9913ec2777
Syrie Bianco <syriebianco@gmail.com>
2017-07-18 10:13:16 -0700

    Create Root and App components, add Provider and HashRouter" "

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Scuba</h1>
+  </div>
+);
+
+export default App;
--- frontend/scuba.jsx
+++ frontend/scuba.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+import Root from './components/root';
+
 //test imports
 import {signup, signin, signout} from './util/session_api_util';
-import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Scuba</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 
 
   // testing
