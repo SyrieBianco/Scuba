451bf189b7cdb5e9b081742f063d68176f3979ab
Syrie Bianco <syriebianco@gmail.com>
2017-07-18 06:29:45 -0700

    Create and test session api calls

--- frontend/scuba.jsx
+++ frontend/scuba.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//test imports
+import {signup, signin, signout} from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+
+  window.signin = signin;
+  window.signup = signup;
+  window.signout = signout;
 });
