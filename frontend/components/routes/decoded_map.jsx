42888ba825cf1c5079d82268510bc2de71382f6b
Syrie Bianco <syriebianco@gmail.com>
2017-07-26 11:03:40 -0700

    Implement minimap for each route on index

---
+++ frontend/components/routes/decoded_map.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class DecodedMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latLngs: google.maps.geometry.encoding.decodePath(
+        this.props.route.waypoints_text)
+    };
+  }
+
+  componentDidMount() {
+    const options = {
+      center: this.state.latLngs[0],
+      zoom: 12,
+      mapTypeControl: false
+    };
+    const map = ReactDOM.findDOMNode(this.refs[`${this.props.route.id}`]);
+    this.map = new google.maps.Map(map, options);
+    this.pathLine = new google.maps.Polyline({
+      strokeColor: '#000000',
+      strokeOpacity: 1,
+      strokeWeight: 3,
+      map: this.map,
+      draggable: false
+    });
+    
+    this.bounds = new google.maps.LatLngBounds();
+    this.state.latLngs.forEach(latLng => {
+      this.pathLine.getPath().push(latLng);
+      this.bounds.extend(latLng)
+    });
+    this.map.fitBounds(this.bounds);
+    this.map.panToBounds(this.bounds);
+
+  }
+
+  render() {
+    const { route } = this.props;
+    const idName = route.id;
+    return(
+      <div id={idName} ref={idName} className="decoded-map"></div>
+    );
+  }
+}
+
+export default DecodedMap;
--- frontend/components/routes/index_item.jsx
+++ frontend/components/routes/index_item.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import DecodedMap from './decoded_map';
 // <img src="https://www.maps.com/media/catalog/product/cache/1/thumbnail/2500x/17f82f742ffe127f42dca9de82fb58b1/2/0/200002_us_political_im_z.jpg" alt={workout.title} />
 class RouteIndexItem extends React.Component {
   constructor(props) {
@@ -15,9 +15,9 @@ class RouteIndexItem extends React.Component {
  render() {
     const { route, destroyRoute } = this.props;
     return (
-
      // <span> { route.elevation_change } </span>
       <li className="route-feed-item">
+        <DecodedMap route={route}/>
         <Link to={`/routes/${route.id}`}>
           <span> Name: { route.name } </span>
           <span> Distance: { route.distance } </span>
